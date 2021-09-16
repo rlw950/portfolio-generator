6097d66c927dc17195a4cec556bbd6ee12b94fd9
Ray Computer <rlw950@yahoo.com>
2021-09-16 12:47:08 -0700

    feature/questions

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+console.log(inquirer)
 
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });

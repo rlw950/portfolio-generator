b45f523cc5d82be144075a67a25d592bf6368bd3
Ray Computer <rlw950@yahoo.com>
2021-10-02 18:57:56 -0700

    feature/finished-site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const generateSite = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
